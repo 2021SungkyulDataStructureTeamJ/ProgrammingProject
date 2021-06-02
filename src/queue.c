cbbfc73f0c38878daa5717b02578211eca5c7474
osm1892 <osm1892@naver.com>
2021-06-02 19:31:10 +0900

    queue 구현

---
+++ src/queue.c
@@ -0,0 +1,32 @@
+﻿#include "queue.h"
+
+Queue *QueueNew()
+{
+	return ListNew();
+}
+
+void Enqueue(Queue *queue, void *data)
+{
+	ListAddNode(queue, ListSize(queue), data);
+}
+
+void Dequeue(Queue *queue)
+{
+	ListDeleteNode(queue, 0);
+}
+
+
+void *QueuePeek(Queue *queue)
+{
+	return ListGetNode(queue, 0);
+}
+
+int QueueSize(Queue *queue)
+{
+	return ListSize(queue);
+}
+
+bool QueueIsEmpty(Queue *queue)
+{
+	return ListIsEmpty(queue);
+}
---
+++ src/queue.h
@@ -0,0 +1,21 @@
+﻿#include "list.h"
+
+typedef LinkedList Queue;
+
+// 새로운 Queue를 생성합니다.
+Queue *QueueNew();
+
+// Queue의 뒤에 데이터를 추가합니다.
+void Enqueue(Queue *queue, void *data);
+
+// Queue맨 앞의 데이터를 삭제합니다.
+void Dequeue(Queue *queue);
+
+// Queue의 맨 앞 데이터를 반환합니다.
+void * QueuePeek(Queue *queue);
+
+// Queue의 사이즈를 반환합니다.
+int QueueSize(Queue *queue);
+
+// Queue가 비어있는지를 반환합니다.
+bool QueueIsEmpty(Queue *queue);
