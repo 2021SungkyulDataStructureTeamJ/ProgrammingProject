c186bc6c4b154350522ab9b8d047628e55b9a5de
osm1892 <osm1892@naver.com>
2021-06-02 23:39:40 +0900

    stack 주석 수정

--- src/stack.h
+++ src/stack.h
@@ -2,23 +2,23 @@
 
 typedef LinkedList Stack;
 
-// 새로운 Queue를 생성합니다.
+// 새로운 Stack을 생성합니다.
 void StackInit(Stack *stack);
 
-// Queue의 뒤에 데이터를 추가합니다.
+// Stack의 뒤에 데이터를 추가합니다.
 void StackPush(Stack *stack, void *data, int size);
 
-// Queue맨 앞의 데이터를 삭제합니다.
+// Stack의 맨 앞의 데이터를 삭제합니다.
 void StackPop(Stack *stack);
 
-// Queue의 맨 앞 데이터를 반환합니다.
+// Stack의 맨 앞 데이터를 반환합니다.
 void *StackPeek(Stack *stack);
 
-// Queue의 사이즈를 반환합니다.
+// Stack의 사이즈를 반환합니다.
 int StackSize(Stack *stack);
 
-// Queue가 비어있는지를 반환합니다.
+// Stack이 비어있는지를 반환합니다.
 bool StackIsEmpty(Stack *stack);
 
-// Queue의 모든 원소를 제거합니다.
+// Stack의 모든 원소를 제거합니다.
 void StackClear(Stack *stack);
